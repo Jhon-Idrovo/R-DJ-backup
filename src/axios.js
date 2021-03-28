dbd9ee7f028596af6ae31873426d4f3a8d9e19d8
Jhon-Idrovo <Jhon_Idrovo3@users.noreply.github.com>
2021-03-28 14:36:48 -0500

    axios fixed

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:8000/";
+const baseURL = "http://react-django-home-finance.herokuapp.com/";
 const axiosInstance = axios.create({
   baseURL: baseURL,
   timeout: 5000,
