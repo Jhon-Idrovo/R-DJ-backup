dba331ea1f0842f95259399eccdb3af29fe718b2
Jhon-Idrovo <Jhon_Idrovo3@users.noreply.github.com>
2021-03-28 15:00:00 -0500

    Secret keys unsafe

--- main_app/settings.py
+++ main_app/settings.py
@@ -149,7 +149,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:3000",
-    "http://Jhon-Idrovo/react-django-home-finance-deployment"
+    "http://react-django-home-finance-deployment"
 ]
 
 #JWT SETTINGS
