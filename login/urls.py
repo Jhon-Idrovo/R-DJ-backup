31e9f955b1fd609f7506a0750d823292d5dd8b9b
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-03-29 21:23:12 -0500

    first push

--- login/urls.py
+++ login/urls.py
@@ -5,5 +5,5 @@ app_name = 'login'
 
 urlpatterns = [
     path('logout/', BlacklistTokenView.as_view(), name='logout'),
-    path('register/', CreateUser.as_view(), template_name='index.html'),
+    path('register/', CreateUser.as_view(template_name='index.html'), name='register'),
 ]
