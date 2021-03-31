104b2ff51dc375ca0fa6e3c1377d4ca94c3ca3ee
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-03-31 12:38:55 -0500

    package.json scripts deleted

--- main_app/urls.py
+++ main_app/urls.py
@@ -33,5 +33,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    re_path('.*', TemplateView.as_view(template_name='index.html', content_type= 'application/javascript'))
+    #re_path('.*', TemplateView.as_view(template_name='index.html', content_type= 'text/html'))
 ]
